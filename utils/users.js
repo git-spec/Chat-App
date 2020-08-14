6c8656418d02eec84acf3c487b015b0558ebedff
felixwurst <felix.wurst@gmail.com>
2020-08-14 16:25:06 +0200

    create database connection for login

--- utils/users.js
+++ utils/users.js
@@ -6,6 +6,7 @@ const runQuery = require('./connection.js');
 
 const users = [];
 
+/* ************************************************************ FUNCTIONS ******************************************************* */
 // Register user
 function registerUser(firstname, lastname, username, email, password) {
   return new Promise((resolve, reject) => {
@@ -19,10 +20,38 @@ function registerUser(firstname, lastname, username, email, password) {
         reject("exists");
       } else {
         reject(err);
-      }
-    })
-  })
-}
+      };
+    });
+  });
+};
+
+// Get user for login
+function getUser(username, password) {
+  return new Promise((resolve, reject) => {
+    runQuery(`SELECT * FROM users WHERE username LIKE '${username}'`).then(users => {
+      // console.log(users);
+      if (users.length === 0) {
+        // user not found
+        reject(3);
+      } else {
+        if (passwordHash.verify(password, users[0].password)) {
+          // add property _id to user
+          // users[0]._id = users[0].id; 
+                    // users[0]._id = users[0].id; 
+
+          // password correct
+          resolve(users[0]);
+        } else {
+          // password not correct
+          reject(3); 
+        }
+      };
+    }).catch(err => {
+      // server error
+      reject(err); 
+    });
+  });
+};
 
 // Join user to chat
 function userJoin(id, username, room) {
@@ -49,10 +78,12 @@ function getRoomUsers(room) {
   return users.filter(user => user.room === room);
 };
 
+/* ************************************************************ EXPORT ******************************************************* */
 module.exports = {
+  registerUser,
+  getUser,
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers,
-  registerUser
+  getRoomUsers
 };
